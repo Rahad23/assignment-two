5c6a11129ebd40a9be1980b3f2677b5aa16739f4
Rahad23 <rahadhasan33675@gmail.com>
2024-05-21 05:44:27 -0700

    make delete product functionality

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,26 +1,27 @@
 import { Product } from "./product.interface";
 import { Product_ } from "./product.model";
 
+//const create product
 const createProductService = async (productData: Product) => {
   const result = await Product_.create(productData);
   return result;
 };
 
-//----------
+//get all product
 
 const getAllProductService = async () => {
   const result = await Product_.find();
   return result;
 };
 
-//----------
+//get one product data
 
 const getOneProductService = async (productId: string) => {
   const result = await Product_.findOne({ _id: productId });
   return result;
 };
 
-//----------
+//update one product with product id
 
 const updateOneProductService = async (productId: string, data: Product) => {
   const result = await Product_.updateOne(
@@ -34,7 +35,7 @@ const updateOneProductService = async (productId: string, data: Product) => {
   return updatedData;
 };
 
-//-----------
+//product delete with product id
 
 const productDeleteWithIdService = async (id: string) => {
   const result = await Product_.deleteOne({ _id: id });
@@ -45,7 +46,7 @@ const productDeleteWithIdService = async (id: string) => {
   return deleteProduct;
 };
 
-//------------
+//search product with product name
 
 const searchProductWithProductNameService = async (productName: string) => {
   const result = await Product_.find({ name: new RegExp(productName, "i") });
